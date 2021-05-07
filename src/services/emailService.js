668dc3e8a53f1328be7bf6bc37277014d0bedadb
anjana6 <anjanashakthi95@gmail.com>
2021-05-07 08:27:14 +0530

    create the email service

---
+++ src/services/emailService.js
@@ -0,0 +1,47 @@
+import * as nodmailer from 'nodemailer';
+import { google } from 'googleapis';
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  '569042713265-2aa4nougdukn643itu0q654r8oh2rjre.apps.googleusercontent.com',
+  'i6S-GJgeUUIcnlkdd85__8vp', // Client Secret
+  'https://developers.google.com/oauthplayground' // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token:
+    '1//048wrHzE8OQhPCgYIARAAGAQSNgF-L9IrL55JhsGWOClAtL0CxVA0zicjMvSjFcRb8otqmVMUza5NzgJMkFTxscGbqqUWR8LyjA',
+});
+const accessToken = oauth2Client.getAccessToken();
+
+const smtpTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: 'shakthi.10jaya@gmail.com',
+    clientId:
+      '569042713265-2aa4nougdukn643itu0q654r8oh2rjre.apps.googleusercontent.com',
+    clientSecret: 'i6S-GJgeUUIcnlkdd85__8vp',
+    refreshToken:
+      '1//048wrHzE8OQhPCgYIARAAGAQSNgF-L9IrL55JhsGWOClAtL0CxVA0zicjMvSjFcRb8otqmVMUza5NzgJMkFTxscGbqqUWR8LyjA',
+    accessToken: accessToken,
+  },
+});
+
+class EmailService {
+  sendMail(email, subject) {
+    const mailOptions = {
+      from: 'shakthi.10jaya@gmail.com',
+      to: email,
+      subject: subject,
+      generateTextFromHTML: true,
+      html: '<b>test</b>',
+    };
+
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+      error ? console.log(error) : console.log(response);
+      smtpTransport.close();
+    });
+  }
+}
