e1459f1ba5bfa4a334352da28becdf231a021ae3
anjana6 <anjanashakthi95@gmail.com>
2021-05-10 23:41:12 +0530

    fix the unlike

--- src/services/likeService.js
+++ src/services/likeService.js
@@ -2,7 +2,7 @@
  * @Author: Anjana (anjanashakthi95@gmail.com)
  * @Date: 2021-04-30 06:20:19
  * @Last Modified by: Anjana (anjanashakthi95@gmail.com)
- * @Last Modified time: 2021-05-10 23:11:56
+ * @Last Modified time: 2021-05-10 23:40:38
  */
 
 import likeRepository from '../repositories/likeRepository';
@@ -22,7 +22,7 @@ class LikeService {
       return likeRepository.createLike(body);
     }
 
-    return likeRepository.deleteLike(userId, postId);
+    return likeRepository.deleteLike(user.userId, postId);
   }
 }
 
