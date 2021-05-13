f852b870dc68007a62756c100a92c34a1c84aac0
anjana6 <anjanashakthi95@gmail.com>
2021-05-13 09:09:45 +0530

    fix the bug of post and user repository

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -2,7 +2,7 @@
  * @Author: Anjana (anjanashakthi95@gmail.com)
  * @Date: 2021-04-30 06:22:03
  * @Last Modified by: Anjana (anjanashakthi95@gmail.com)
- * @Last Modified time: 2021-05-10 23:35:39
+ * @Last Modified time: 2021-05-12 22:31:34
  */
 
 const Post = require('../models').posts;
@@ -27,7 +27,10 @@ class PostRepository {
   fetchPosts() {
     return Post.findAll({
       include: [
-        { model: User },
+        {
+          model: User,
+          include: [{ model: Profile, attributes: ['profile_url'] }],
+        },
         {
           model: Comment,
           include: [
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -2,7 +2,7 @@
  * @Author: Anjana (anjanashakthi95@gmail.com)
  * @Date: 2021-04-30 06:22:54
  * @Last Modified by: Anjana (anjanashakthi95@gmail.com)
- * @Last Modified time: 2021-05-10 09:58:00
+ * @Last Modified time: 2021-05-12 22:18:47
  */
 
 const Sequelize = require('sequelize');
@@ -25,6 +25,7 @@ class UserRepository {
 
   fetchUserByEmail(email) {
     return User.findOne({
+      include: [{ model: Profile }],
       where: {
         email: email,
       },
